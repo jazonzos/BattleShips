7fb2e623768ad7d3efbd3b59144b98f610c73c9d
jazonzos <1778064@student.swin.edu.au>
2015-10-14 18:01:27 +1100

    update

--- src/Model/AIEasyPlayer.cs
+++ src/Model/AIEasyPlayer.cs
@@ -3,81 +3,120 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-//using System.Data;
+// using System.Data;
 using System.Diagnostics;
 
 /// <summary>
 /// EasyAIPlayer is a type of AIPlayer. All shot that are taken are random
 /// shot generated witht the GenerateCoords
 /// </summary>
+
 [Serializable]
 public class AIEasyPlayer : AIPlayer
 {
-[Serializable]
+	/// <summary>
+	/// Private enumarator for AI states. currently there are two states,
+	/// the AI can be searching for a ship, or if it has found a ship it will
+	/// target the same ship
+	/// </summary>
+	private enum AIStates
+	{
+		Searching,
+		TargetingShip
+	}
+
+	private AIStates _CurrentState = AIStates.Searching;
+
+	private Stack<Location> _Targets = new Stack<Location>();
 	public AIEasyPlayer(BattleShipsGame controller) : base(controller)
 	{
 	}
 
-
-	private Location _nextLocation;
 	/// <summary>
-	/// GenerateCoords generates shots at random location.
+	/// GenerateCoordinates should generate random shooting coordinates
 	/// </summary>
-	/// <param name="row">the row it generates</param>
-	/// <param name="column">the column it generates</param>
+	/// <param name="row">the generated row</param>
+	/// <param name="column">the generated column</param>
 	protected override void GenerateCoords(ref int row, ref int column)
 	{
 		do {
-			if (_nextLocation != null) {
-				row = _nextLocation.Row;
-				column = _nextLocation.Column;
-				_nextLocation = null;
-			} else {
-				row = Player._Random.Next(0, EnemyGrid.Height);
-				column = _Random.Next(0, EnemyGrid.Width);
+			//check which state the AI is in and uppon that choose which coordinate generation
+			//method will be used.
+			switch (_CurrentState) {
+				case AIStates.Searching:
+					SearchCoords(ref row, ref column);
+					break;
+				case AIStates.TargetingShip:
+					SearchCoords(ref row, ref column);
+					break;
+				default:
+					throw new ApplicationException("AI has gone in an imvalid state");
 			}
-		} while (EnemyGrid[row, column] != TileView.Sea);
+		} while ((row < 0 || column < 0 || row >= EnemyGrid.Height || column >= EnemyGrid.Width || EnemyGrid[row, column] != TileView.Sea));
+		//while inside the grid and not a sea tile do the search
 	}
 
 	/// <summary>
-	/// ProcessShot does nothing with the easy AIPlayer because this player
-	/// doesn't know anything beside generating random coordinates
+	/// TargetCoords is used when a ship has been hit and it will try and destroy
+	/// this ship
 	/// </summary>
-	/// <param name="row">row it shot at</param>
-	/// <param name="col">the col it shot at</param>
-	/// <param name="result">the result from the last shot</param>
+	/// <param name="row">row generated around the hit tile</param>
+	/// <param name="column">column generated around the hit tile</param>
+	private void TargetCoords(ref int row, ref int column)
+	{
+		Location l = _Targets.Pop();
+
+		if ((_Targets.Count == 0))
+			_CurrentState = AIStates.Searching;
+		row = l.Row;
+		column = l.Column;
+	}
+
+	/// <summary>
+	/// SearchCoords will randomly generate shots within the grid as long as its not hit that tile already
+	/// </summary>
+	/// <param name="row">the generated row</param>
+	/// <param name="column">the generated column</param>
+	private void SearchCoords(ref int row, ref int column)
+	{
+		row = _Random.Next(0, EnemyGrid.Height);
+		column = _Random.Next(0, EnemyGrid.Width);
+	}
+
+	/// <summary>
+	/// ProcessShot will be called uppon when a ship is found.
+	/// It will create a stack with targets it will try to hit. These targets
+	/// will be around the tile that has been hit.
+	/// </summary>
+	/// <param name="row">the row it needs to process</param>
+	/// <param name="col">the column it needs to process</param>
+	/// <param name="result">the result og the last shot (should be hit)</param>
+
 	protected override void ProcessShot(int row, int col, AttackResult result)
 	{
 		if (result.Value == ResultOfAttack.Hit) {
-			switch (_Random.Next(4)) {
-				case 0:
-					_nextLocation = new Location(row + 1, col);
-					break;
-				case 1:
-					_nextLocation = new Location(row - 1, col);
-					break;
-				case 2:
-					_nextLocation = new Location(row, col + 1);
-					break;
-				case 3:
-					_nextLocation = new Location(row, col - 1);
-					break;
-			}
-
-			if (_nextLocation.Row < 0) {
-				_nextLocation.Row = 0;
-			} else if (_nextLocation.Row >= PlayerGrid.Height) {
-				_nextLocation.Row = PlayerGrid.Height - 1;
-			}
-
-			if (_nextLocation.Column < 0) {
-				_nextLocation.Column = 0;
-			} else if (_nextLocation.Column >= PlayerGrid.Width) {
-				_nextLocation.Column = PlayerGrid.Width - 1;
-			}
+			_CurrentState = AIStates.TargetingShip;
+			AddTarget(row - 1, col);
+			AddTarget(row, col - 1);
+			AddTarget(row + 1, col);
+			AddTarget(row, col + 1);
+		} else if (result.Value == ResultOfAttack.ShotAlready) {
+			throw new ApplicationException("Error in AI");
 		}
 	}
 
+	/// <summary>
+	/// AddTarget will add the targets it will shoot onto a stack
+	/// </summary>
+	/// <param name="row">the row of the targets location</param>
+	/// <param name="column">the column of the targets location</param>
+	private void AddTarget(int row, int column)
+	{
+
+		if (row >= 0 && column >= 0 && row < EnemyGrid.Height && column < EnemyGrid.Width && EnemyGrid[row, column] == TileView.Sea) {
+			_Targets.Push(new Location(row, column));
+		}
+	}
 }
 
 //=======================================================
